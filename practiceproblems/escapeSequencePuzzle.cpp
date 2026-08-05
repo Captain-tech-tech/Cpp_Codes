a80996a3a05abbd62a8c750a02f39b680e5699b3
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-05 06:55:56 +0500

    this code show the use of escape sequences

---
+++ practiceproblems/escapeSequencePuzzle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Path to GCC Compiler : C:\\w64devkit\\bin\\gcc.exe";
+    cout<<endl<<"I said, \"Let's learn C++!\""<<endl;
+    cout<<"List of tasks:"<<endl;
+    cout<<"\t1. Install compiler"<<endl;
+    cout<<"\t2. Write first prigram"<<endl;
+    cout<<"\t3. Compile and Run"<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
