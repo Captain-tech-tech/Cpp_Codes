a0ceba611e9ba113259b1918827a738f1b8231e3
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-05 08:07:33 +0500

    code for type casting an integer into string and adding % sign to that string

---
+++ practiceproblems/stringTypeCast.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int a = 75;
+    string s = to_string(a) + "%";
+    cout<<s<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
