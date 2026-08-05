ce2cd8a58e9f075cc0de31a98589e734cf6fa2fb
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-05 07:17:07 +0500

    printing the roll number in a specified manner

---
+++ practiceproblems/rollnumberdisplay.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str = "My Roll Number";
+    int first = 25;
+    char campus = 'P';
+    char c = '-';
+    string last = "0053";
+    cout<<str<<" : "<<first<<c<<campus<<c<<last<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
