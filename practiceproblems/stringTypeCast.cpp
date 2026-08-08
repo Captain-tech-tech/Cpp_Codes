24e6750d1051496a25b1f5342729721b7c531c31
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-08 11:37:33 +0500

    added a code for finding the length of a string through build in function

--- practiceproblems/stringTypeCast.cpp
+++ practiceproblems/stringTypeCast.cpp
@@ -1,19 +1,38 @@
-#include<iostream>
-#include<string>
-using namespace std;
-int main()
-{
-    int a = 75;
-    string s = to_string(a) + "%";
-    cout<<s<<endl;
-    return 0;
-}
+// string type casting
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// int main()
+// {
+//     int a = 75;
+//     string s = to_string(a) + "%";
+//     cout<<s<<endl;
+//     return 0;
+// }
 
 
 
 
+// finding string length through build-in-function
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// int main() {
+//     string str = "Hello";
+//     cout << "Length: " << str.length() << endl;
+//     return 0;
+// }
 
+// or 
 
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+// int main() {
+//     char str[] = "Hello";
+//     cout << "Length: " << strlen(str) << endl;
+//     return 0;
+// }
 
 
 
