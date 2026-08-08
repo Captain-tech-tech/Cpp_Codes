488a5f462c641017c667ab888ea48aa818a3a8e1
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-08 11:06:15 +0500

    sorting of array through bubble sorting

---
+++ practiceproblems/sortingOfArray.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;cout<<"Enter the size of  your array :";cin>>n;
+    int arr[n];
+
+    cout<<"Enter "<<n<<" elements for your array :";
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+
+    cout<<"----- Sorting of array -----"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    cout<<endl<<"Sorted array :";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<"  ";
+    }
+    cout<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
