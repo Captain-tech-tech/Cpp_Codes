c421bb2bc5d2c0e87d7bac756b970a788f7af13f
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-19 07:37:36 +0500

    accesessing array by pointer and printing values of array elements through pointer

---
+++ pointers/pointer_to_array.cpp
@@ -0,0 +1,17 @@
+
+// 2
+// pointer to an array
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int array[] = {457,57,34,456,34,562,234,45,235};
+
+//     int *ptr = array;
+//     cout<<&array[0]<<"      "<<&array<<"      "<<ptr<<endl;
+
+//     for(int i=0; i<9; i++)
+//     cout<<*(ptr+i)<<" ";
+//     cout<<endl;
+//     return 0;
+// }
