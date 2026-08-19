4e7f2f771ccc4324b3a288fa31b6e4fd99a7e134
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-19 07:39:02 +0500

    array reversal through pointer

---
+++ pointers/arrayReversal.cpp
@@ -0,0 +1,26 @@
+// 4 
+// Reversing an array using pointers
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int s;cout<<"Enter the size of the array : ";cin>>s;
+//     int array[s];
+//     cout<<"Enter "<<s<<" numbers for your array : ";
+
+//     for(int i=0;i<s;i++)
+//         cin>>array[i];
+
+//     int *ptr = array;
+
+//     cout<<"Your array : ";
+//     for(int i = 0;i<s;i++)
+//         cout<<*(ptr+i)<<"  ";
+
+//         cout<<endl<<"Reversed array : ";
+//     for(int i=s-1;i >= 0;i--)
+//         cout<<*(ptr+i)<<"  ";
+
+//         cout<<endl;
+//     return 0;
+// }
