e445319ffe0f072e0b338d8a2790d87d4b62a36c
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-19 07:38:18 +0500

    swipping two numbers using pointers without using a third variable

---
+++ pointers/swipping.cpp
@@ -0,0 +1,19 @@
+// 3
+// swipping two numbers using pointers without using a third variable
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 454, b = 982;
+//     int *ptr1 = &a, *ptr2 = &b;
+
+//     cout<<"Before Swipping  ---->  a : "<<a<<",  b : "<<b<<endl;
+
+//     *ptr1 = *ptr1+*ptr2;
+//     *ptr2 = *ptr1 - *ptr2;
+//     *ptr1 = *ptr1 - *ptr2;
+
+//     cout<<"After Swipping   ---->  a : "<<a<<",  b : "<<b<<endl;
+
+//     return 0;
+// }
