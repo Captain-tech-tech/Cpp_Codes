9334e8635ca3d89242e66a8beffd6203fac520ec
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-19 07:36:22 +0500

    storing address of a varibale in a pointer and increment and decrement through pointer

---
+++ pointers/basic.cpp
@@ -0,0 +1,13 @@
+// 1
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 345;
+//     int* ptr = &a;
+//     cout<<"a : "<<a<<endl<<"*ptr : "<<*ptr<<endl;
+//     (*ptr)++;
+//     (*ptr)-=45;
+//     cout<<a<<endl;
+//     return 0;
+// }
